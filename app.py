d91e7f862353b72da04451140d93ad838abd68df
silvia <villalobos.veas@gmail.com>
2022-11-11 11:04:12 -0300

    render template

--- app.py
+++ app.py
@@ -2,17 +2,21 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
-
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hola Mundo'
+def hello_world():
+    return render_template('index.html')
 
 
 @app.route('/usuario/<name>')
 def user(name):
     return render_template('user.html', name = name)
+
+
+@app.route('/usuario')
+def user_incognito():
+    return render_template('user.html')    
 
 
 @app.route('/navegador')
