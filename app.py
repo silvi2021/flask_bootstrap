bfd032625c9206f00fb5a0439346fbe9f25554f7
silvia <villalobos.veas@gmail.com>
2022-11-18 11:04:06 -0300

    actualizar y agregar foto

--- app.py
+++ app.py
@@ -2,21 +2,18 @@ from flask import Flask, request, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] ="CUALQUIER COSA"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:cata@localhost:5432/flask_bootstrap'
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 class Message(db.Model):
-    __tablename__ = 'messages'
-
-    
+    __tablename__ = 'messages'    
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128), nullable=False)
     content = db.Column(db.Text, nullable=False)
+    picture = db.Column(db.String(300))
 
     def __repr__(self):
         return f'<Message {self.title}>'
@@ -31,19 +28,31 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
+        picture = request.form['picture']
         
         if not title:
             flash('El titulo es obligatorio')
         elif not content:
             flash('El contenido es obligatorio') 
         else:
-            message = Message(title = title, content = content)
+            message = Message(title = title, content = content, picture = picture)
             db.session.add(message)
             db.session.commit()
             return redirect(url_for('index'))
                    
     return render_template('create.html')
 
+@app.route('/<id>/update', methods = ('GET', 'POST'))
+def update(id):
+    message = Message.query.filter_by(id = id).first()
+    if request.method == 'POST':
+        if message:
+            message.title = request.form['title']  
+            message.content = request.form['content'] 
+            message.picture = request.form['picture']
+            db.session.commit() 
+            return redirect('/')
+    return render_template('update.html', message = message)
 
 
 @app.route('/usuario/<name>')
